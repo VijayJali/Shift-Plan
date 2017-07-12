336e1d7f3e7142c360e847b0cd90870731be9aa8
Vijay Reddy Jali <vijayreddy.jali@gmail.com>
2017-07-12 10:25:38 -0400

    Update Automation-Shift-Plan.py

--- Automation-Shift-Plan.py
+++ Automation-Shift-Plan.py
@@ -1,1 +1,9 @@
-import os
+Ranges of cells can be accessed using slicing
+
+>>> cell_range = ws['A1':'C2']
+Ranges of rows or columns can be obtained similarly:
+
+>>> colC = ws['C']
+>>> col_range = ws['C:D']
+>>> row10 = ws[10]
+>>> row_range = ws[5:10]
